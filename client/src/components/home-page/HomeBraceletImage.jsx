e8fec6e434133eebf8ac551c772ab1db7a84d33c
Alex Cholmsky <acholmsk@uwaterloo.ca>
2023-11-20 22:29:47 -0500

    Added decal to put engraving on wood centerpiece

--- client/src/components/home-page/HomeBraceletImage.jsx
+++ client/src/components/home-page/HomeBraceletImage.jsx
@@ -5,7 +5,7 @@ export default function HomeBraceletImage() {
     <div className="bg-white justify-end max-w-4xl">
       <div style={{ marginLeft: '8rem' }}>
         <img
-          src="public/images/TraitletNoBorder.png"
+          src="./images/three_traitlets_homepage.png"
           alt="Triatlet Background"
           style={{ maxWidth: '100%', display: 'block', margin: '12rem auto' }}
         />
