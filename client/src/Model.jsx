9d099c70cf1604efea4ea78773a03d4944975377
Alex Cholmsky <acholmsk@uwaterloo.ca>
2023-11-29 22:17:28 -0500

    remove unneeded files and imports

--- client/src/Model.jsx
+++ client/src/Model.jsx
@@ -5,9 +5,6 @@ Command: npx gltfjsx@6.2.15 ../public/model2.glb
 import React, { useEffect, useState, useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import {PlaneGeometry, MeshStandardMaterial, TextureLoader, CanvasTexture} from 'three';
-import { useSelectionContext } from './context/SelectionContext.jsx';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF('/model2.glb');
