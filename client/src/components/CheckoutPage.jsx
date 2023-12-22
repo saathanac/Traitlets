93d1c37c6f2438d66dd5b37592e753eca5e1ae56
saathanac <108774535+saathanac@users.noreply.github.com>
2023-12-22 14:42:32 -0500

    adding hamburger to all pages

--- client/src/components/CheckoutPage.jsx
+++ client/src/components/CheckoutPage.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import OrderSummary from './checkout-page/OrderSummary'
 import PaymentDetails from './checkout-page/PaymentDetails'
+import HomeHamburger from './home-page/HomeHamburger'
 
 const CheckoutPage = (props) => {
   return (
       <div className="absolute lg:flex flex-col lg:flex-row w-full h-full">
-      <div className="lg:w-1/2 w-full bg-white">
-      <OrderSummary/>
-      </div>
-      <div className="lg:w-1/2 w-full bg-gray-200">
-      <PaymentDetails/>
-      </div>
+        <div className='absolute top-2 right-2 z-50'>
+          <HomeHamburger/>
+        </div>
+        <div className="lg:w-1/2 w-full bg-white">
+          <OrderSummary/>
+        </div>
+        <div className="lg:w-1/2 w-full bg-gray-200">
+          <PaymentDetails/>
+        </div>
     </div>
   )
 }
