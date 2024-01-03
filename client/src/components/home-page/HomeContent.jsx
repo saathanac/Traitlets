2c8e7019a87ed861420d2fa0d7e8778f4f54c2d6
Alex Cholmsky <acholmsk@uwaterloo.ca>
2024-01-03 18:54:14 -0500

    homepage image

--- client/src/components/home-page/HomeContent.jsx
+++ client/src/components/home-page/HomeContent.jsx
@@ -5,8 +5,11 @@ import HomeBraceletImage from './HomeBraceletImage';
 function HomeContent() {
   return (
     <div className="overflow-hidden flex-col flex lg:flex-row w-full bg-white">
-      <div className="w-full h-full bg-white">
+      <div className="lg:w-1/2 w-full bg-white">
         <HomeWelcome />
+      </div>
+      <div className="lg:w-1/2 h-full bg-white">
+        <HomeBraceletImage />
       </div>
     </div>
   );
